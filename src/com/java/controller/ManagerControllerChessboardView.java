2b90cb8e553ef34abe3d7dd4b4e40356fc9356e1
Miguel991 <python_hmc@hotmail.com>
2017-04-10 12:14:21 -0300

    Board Controller

---
+++ src/com/java/controller/ManagerControllerChessboardView.java
@@ -0,0 +1,7 @@
+package com.java.controller;
+
+public class ManagerControllerChessboardView {
+	
+	// Interactuar con  el manager de piezas y el player para pasarle las vistar de forma ordenada a la vista tablero
+
+}
