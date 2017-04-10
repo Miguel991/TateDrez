23c173724f5300a02ea5469d602397e7b61d511e
Miguel991 <python_hmc@hotmail.com>
2017-04-09 23:19:29 -0300

    Controller implementation

---
+++ src/com/java/controller/PieceController.java
@@ -0,0 +1,7 @@
+package com.java.controller;
+
+public class PieceController {
+	
+	//interactuar con las vistas de las piezas y ponerlas en la coordenada corespondiente en la vista del tablero
+
+}
---
+++ src/com/java/controller/PlayerController.java
@@ -0,0 +1,8 @@
+package com.java.controller;
+
+public class PlayerController {
+	
+	//le dice a la clase jugador del modelo que pieza quiere mover el usuario
+	
+
+}
