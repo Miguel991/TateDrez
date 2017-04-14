09329ec3b1b649409b2c29dbee590f7e55ef30cd
Miguel991 <python_hmc@hotmail.com>
2017-04-14 11:40:30 -0300

    Logic of the board view

--- src/com/java/view/RookView.java
+++ src/com/java/view/RookView.java
@@ -7,11 +7,18 @@ import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-
 import javax.swing.JPanel;
 
+/**
+ * @author HMC Software Development
+ *
+ */
 public class RookView extends JPanel implements MouseListener{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7324898131582648993L;
 	private String name = "";
 	private Image  imagenRook;
 	private boolean rookSelect = false;
@@ -21,9 +28,11 @@ public class RookView extends JPanel implements MouseListener{
 	private ManagerResources manager = ManagerResources.getInstance();
 	
 	public RookView(int x, int y,String name){
+		this.name = name;
 		this.point = new Point(x,y);
 		imagenRook = manager.getImagenRook();
 		rectangle = new Rectangle(this.point,new Dimension(40,70));
+		setFocusable(true);
 		savePiece(this);
 	}
 
@@ -33,9 +42,39 @@ public class RookView extends JPanel implements MouseListener{
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+		//at this point we would have to call the controller
+		for(RookView list: listRook){
+			if(list.getRectangle().contains(e.getPoint())){
+				System.out.println("******************************************");
+				System.out.println("You choice the piece: "+ list.getName());
+				if(list.conditionOfPiece() == false){
+					list.selectedPiece(list);
+				}else{
+					list.deselectedPiece(list);
+				}
+			}
+		}
+		
 	}
 	
+	public void deselectedPiece(RookView rook){
+		this.rookSelect = false;
+		System.out.println("the piece was Deselected: "+rook.getName());
+	}
+	
+	public void selectedPiece(RookView rook){
+		this.rookSelect = true;
+		System.out.println("The piece was selected: "+rook.getName());
+	}
+	
+	public boolean conditionOfPiece(){
+		return this.rookSelect;
+	}
+	
+	private Rectangle getRectangle() {
+		return this.rectangle;
+	}
+
 	public Point getPoint() {
 		return this.point;
 	}
@@ -44,14 +83,18 @@ public class RookView extends JPanel implements MouseListener{
 		return this.imagenRook;
 	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {}
-	@Override
-	public void mouseReleased(MouseEvent e) {}
-	@Override
-	public void mouseEntered(MouseEvent e) {}
-	@Override
-	public void mouseExited(MouseEvent e) {}
+	public String getName() {
+		return this.name;
+	}
 
-} //1139-471007
 
+	@Override
+	public void mousePressed(MouseEvent e){}
+	@Override
+	public void mouseReleased(MouseEvent e){}
+	@Override
+	public void mouseEntered(MouseEvent e){}
+	@Override
+	public void mouseExited(MouseEvent e){}
+
+}
---
+++ src/com/java/view/knightView.java
@@ -0,0 +1,31 @@
+package com.java.view;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+public class knightView extends JPanel implements MouseListener {
+
+	
+	
+	public knightView(){
+		
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+}
